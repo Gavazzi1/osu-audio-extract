df80924949722d6a3b622fafaff309aabc1fdab7
Anthony Gavazzi <gavazzi.a@FACNVY7P14XPD.tld>
2023-11-13 08:34:19 +0100

    use awk to remove space instead of xargs. quiet file removal. comments

--- getaudio.sh
+++ getaudio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm *.osz
+rm -f *.osz
 mkdir -p tmp
-rm tmp/*
+rm -f tmp/*
 unzip -q $1
 newdir="$(echo $1 | cut -d'.' -f1)"
 mkdir -p $newdir
@@ -10,11 +10,13 @@ mkdir -p $newdir
 for i in *.osz; do
     unzip -q "$i" -d tmp
     audiofn="$(grep AudioFilename tmp/*.osu | head -n 1 | rev | cut -d':' -f1 | rev)"
-    audiofn="$(echo $audiofn | xargs)"
-    audiofn="$(echo $audiofn | rev | cut -c2- | rev)"
+    audiofn="$(echo $audiofn | awk '{$1=$1};1')"  # remove leading/trailing space
+    audiofn="$(echo $audiofn | rev | cut -c2- | rev)"  # remove trailing \r
+
     songname="$(grep Title: tmp/*.osu | head -n 1 | rev | cut -d':' -f1 | rev)"
-    songname="$(echo $songname | xargs)"
+    songname="$(echo $songname | awk '{$1=$1};1')"
     songname="$(echo $songname | rev | cut -c2- | rev)"
+
     ext="$(echo $audiofn | rev | cut -d'.' -f1 | rev)"
     echo "$songname - $audiofn"
 
