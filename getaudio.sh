0e8f78bddbd47c0ee193560a860f4bcbde1e3ee6
Anthony <anthonygavaz@yahoo.com>
2024-06-13 15:51:32 -0400

    handle colon in song title

--- getaudio.sh
+++ getaudio.sh
@@ -9,19 +9,21 @@ mkdir -p "$newdir"
 
 for i in *.osz; do
     unzip -q "$i" -d tmp
-    audiofn="$(grep AudioFilename tmp/*.osu | head -n 1 | rev | cut -d':' -f1 | rev)"
+    audiofn="$(grep -h AudioFilename tmp/*.osu | head -n 1 | rev | cut -d':' -f1 | rev)"
     audiofn="$(echo $audiofn | awk '{$1=$1};1')"  # remove leading/trailing space
     audiofn="$(echo $audiofn | rev | cut -c2- | rev)"  # remove trailing \r
 
-    songname="$(grep Title: tmp/*.osu | head -n 1 | rev | cut -d':' -f1 | rev)"
+    titleline="$(grep -h Title: tmp/*.osu | head -n 1)"
+    songname="${titleline##Title:}"
     songname="$(echo $songname | awk '{$1=$1};1')"
     songname="$(echo $songname | rev | cut -c2- | rev)"
+    songname="${songname///}" # remove forward slashes (illegal in linux)
 
     ext="$(echo $audiofn | rev | cut -d'.' -f1 | rev)"
     echo "$songname - $audiofn"
 
     cp "tmp/$audiofn" "$newdir/$songname.$ext"
 
-    rm -r tmp/*
+    rm -r tmp
 done
 rm *.osz
